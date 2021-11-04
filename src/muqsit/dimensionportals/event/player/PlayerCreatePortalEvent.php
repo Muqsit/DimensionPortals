7c91581b9fc2a0f6a03c389f3aa433a5dbcfb515
Muqsit <hackhack05@gmail.com>
2021-11-04 13:19:34 +0000

    Remove unused PlayerCreatePortalEvent::$affected_blocks property

--- src/muqsit/dimensionportals/event/player/PlayerCreatePortalEvent.php
+++ src/muqsit/dimensionportals/event/player/PlayerCreatePortalEvent.php
@@ -14,17 +14,10 @@ use pocketmine\world\Position;
 class PlayerCreatePortalEvent extends DimensionPortalsEvent implements Cancellable{
 	use CancellableTrait;
 
-	private Player $player;
-
-	private Position $block_pos;
-
-	/** @var Block[] */
-	private array $affected_blocks;
-
-	public function __construct(Player $player, Position $block_pos){
-		$this->player = $player;
-		$this->block_pos = $block_pos;
-	}
+	public function __construct(
+		private Player $player,
+		private Position $block_pos
+	){}
 
 	final public function getPlayer() : Player{
 		return $this->player;
